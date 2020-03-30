b02d507ea5903d1a89fac18d364d67a039b8e53e
safronman <safronmanbox@gmail.com>
2020-03-30 16:26:52 +0300

    Refactoring todolist service. Delete duplicate

--- src/app/shared/services/todolist.service.ts
+++ src/app/shared/services/todolist.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {ITodo} from '../../home/home.component';
 import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
 
 interface IAddTodoResponse {
     data: {
@@ -17,38 +18,29 @@ interface IDeleteTodoResponse {
     resultCode: number;
 }
 
-
 @Injectable({
     providedIn: 'root'
 })
 export class TodolistService {
 
+    options = {
+        withCredentials: true,
+        headers: new HttpHeaders().append('API-KEY', '794181ab-6d62-4cfb-bc9f-d539dfac55f1')
+    };
+
     constructor(private http: HttpClient) {
     }
 
     getTodolists(): Observable<ITodo[]> {
-        return this.http.get<ITodo[]>('https://social-network.samuraijs.com/api/1.0/todo-lists',
-            {
-                withCredentials: true,
-                headers: new HttpHeaders().append('API-KEY', '794181ab-6d62-4cfb-bc9f-d539dfac55f1')
-            });
+        return this.http.get<ITodo[]>(environment.baseUrl, this.options);
     }
 
     addTodolist(value: string): Observable<IAddTodoResponse> {
-        return this.http.post<IAddTodoResponse>('https://social-network.samuraijs.com/api/1.0/todo-lists',
-            {title: value},
-            {
-                withCredentials: true,
-                headers: new HttpHeaders().append('API-KEY', '794181ab-6d62-4cfb-bc9f-d539dfac55f1')
-            });
+        return this.http.post<IAddTodoResponse>(environment.baseUrl, {title: value}, this.options);
     }
 
     deleteTodolist(todolistId: string): Observable<IDeleteTodoResponse> {
-        return this.http.delete<IDeleteTodoResponse>(`https://social-network.samuraijs.com/api/1.0/todo-lists/${todolistId}`,
-            {
-                withCredentials: true,
-                headers: new HttpHeaders().append('API-KEY', '794181ab-6d62-4cfb-bc9f-d539dfac55f1')
-            });
+        return this.http.delete<IDeleteTodoResponse>(`${environment.baseUrl}/${todolistId}`, this.options);
     }
 
 }
