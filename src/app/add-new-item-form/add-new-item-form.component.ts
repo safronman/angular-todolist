b6312f7b9aa9d508b8864caeb97b4a4790d3f40a
safronman <safronmanbox@gmail.com>
2020-03-30 11:43:23 +0300

    Add different styles to add-new-item-form.component  with ngClass and @Input()

--- src/app/add-new-item-form/add-new-item-form.component.ts
+++ src/app/add-new-item-form/add-new-item-form.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-add-new-item-form',
-  templateUrl: './add-new-item-form.component.html',
-  styleUrls: ['./add-new-item-form.component.css']
+    selector: 'app-add-new-item-form',
+    templateUrl: './add-new-item-form.component.html',
+    styleUrls: ['../app.component.css',  './add-new-item-form.component.css']
 })
 export class AddNewItemFormComponent implements OnInit {
 
-  constructor() { }
+    @Input() isInputBig = false;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
