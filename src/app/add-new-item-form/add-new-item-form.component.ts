991f1f077688d326a527ff8ec9739cdbb7a88b07
safronman <safronmanbox@gmail.com>
2020-03-30 11:03:22 +0300

    Create components and write css styles

---
+++ src/app/add-new-item-form/add-new-item-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-item-form',
+  templateUrl: './add-new-item-form.component.html',
+  styleUrls: ['./add-new-item-form.component.css']
+})
+export class AddNewItemFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {TodolistComponent} from './todolist/todolist.component';
+import {TaskComponent} from './task/task.component';
+import {HomeComponent} from './home/home.component';
+import {AddNewItemFormComponent} from './add-new-item-form/add-new-item-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TodolistComponent,
+        TaskComponent,
+        HomeComponent,
+        AddNewItemFormComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
